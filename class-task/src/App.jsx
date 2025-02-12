dcb6eeae3ab063c78ceedfc17bd5aa77cd905973
SurkhayliShukur <shukursurkhayli000@gmail.com>
2025-02-12 11:12:58 +0400

    reusable component

--- class-task/src/App.jsx
+++ class-task/src/App.jsx
@@ -1,16 +1,24 @@
 import React from 'react'
-import Header from './componets/Header'
+// import Header from './componets/Header'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from './componets/Button';
+// import Navbars from './componets/Navbar';
 
 
 function App() {
-
+  const handleClick = (message) => {
+    alert(message);
+  };
 
   return (
-    <div>
-      <Header />
-    </div>
-  )
+    <>
+      <div className='d-flex justify-start align-items-center p-3'>
+        <Button label="Primary-button" onClick={() => handleClick('Primary button clicked!')} style="bg-primary" />
+        <Button label="Secondary-button" onClick={() => handleClick('Secondary button clicked!')} style="bg-secondary" />
+        <Button label="Danger-button" onClick={() => handleClick('Danger button clicked!')} style="bg-danger" />
+      </div>
+    </>
+  );
 }
 
 export default App
--- class-task/src/componets/Button/index.jsx
+++ class-task/src/componets/Button/index.jsx
@@ -1,13 +1,25 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-const Button = ({ label, onClick }) => {
+const Button = ({ label, onClick, style }) => {
   return (
     <button
       onClick={onClick}
+      className={`p-3 text-white  ${style}`}
     >
       {label}
     </button>
   );
 };
-
 export default Button;
+
+Button.propTypes = {
+  label: PropTypes.string.isRequired,
+  onClick: PropTypes.func.isRequired,
+  style: PropTypes.string,
+};
+
+Button.defaultProps = {
+  style: "",
+};
+
